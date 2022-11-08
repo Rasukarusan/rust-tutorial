886566726e1f803cc5dbd659d630b74d90198432
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-11-08 12:02:20 +0900

    add code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("数を当ててご覧");
+
+    println!("ほら、予想を入力してね");
+
+    let mut guess = String::new();
+    
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("行の読み込みに失敗しました");
+
+    println!("次のように予想しました： {}", guess);
 }
